76f27053c235a49d4b62a156f12b00dea8f37835
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-05 02:25:11 +0600

    successfully recived movie data to frontend

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// middelware
+app.use(cors());
+app.use(express.json())
+
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdxj8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ph10.ehebq.mongodb.net/?retryWrites=true&w=majority&appName=ph10`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const movieCollection = client.db("movieDB").collection("movies");
+
+    // post movie in db
+    app.post('/movies', async (req, res) => {
+      const movies = req.body;
+      const result = await movieCollection.insertOne(movies);
+      res.send(result);
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// root view on server is run
+app.get("/", (req, res) => {
+  res.send("Server is runing")
+})
+
+app.listen(port, () => {
+  console.log(`Server is runing on port: ${port}`);
+})
