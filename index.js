70112ef2547bcf799c74c5ff457c59f01779addf
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-07 14:56:29 +0600

    implement update function

--- index.js
+++ index.js
@@ -30,11 +30,27 @@ async function run() {
 
     const movieCollection = client.db("movieDB").collection("movies");
     const userCollection = client.db("movieDB").collection("users");
+    const favoriteMovieCollection = client.db("movieDB").collection("favoritemovie")
 
     // create movie in db
     app.post('/movies', async (req, res) => {
       const movies = req.body;
       const result = await movieCollection.insertOne(movies);
+      res.send(result);
+    })
+
+    // create favorite movie in db
+    app.post("/favoritemovie", async (req, res) => {
+      const movie = req.body;
+      const result = await favoriteMovieCollection.insertOne(movie);
+      res.send(result);
+    })
+
+
+    // read favorite movie from db
+    app.get("/favoritemovie", async (req, res) => {
+      const cursor = favoriteMovieCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
     })
 
@@ -47,8 +63,15 @@ async function run() {
 
     // read movie from db
     app.get("/movies", async (req, res) => {
-      const cursor = movieCollection.find();
-      const result = await cursor.toArray();
+      const { searchParams } = req.query;
+
+      let option = {};
+
+      if (searchParams) {
+        option = { title: { $regex: searchParams, $options: "i" } }
+      }
+
+      const result = await movieCollection.find(option).toArray();
       res.send(result);
     })
 
@@ -58,6 +81,26 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const movie = await movieCollection.findOne(query);
       res.send(movie)
+    })
+
+    // update data 
+    app.patch("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          image: data?.image,
+          title: data?.title,
+          genre: data?.genre,
+          duration: data?.duration,
+          year: data?.year,
+          rating: data?.rating,
+          summery: data?.summery,
+        }
+      }
+      const result = await movieCollection.updateOne(query, update);
+      res.send(result)
     })
 
     // delete single data form db
